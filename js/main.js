0feb4478c6511d3a81759a24b62d25b66ad2cdea
Ilham <48025760+ipirm@users.noreply.github.com>
2019-05-31 04:30:58 +0400

    New styles added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
     var swiper = new Swiper('.inner-slider', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-		 pagination: {
-        el: '.swiper-pagination',
-      },
+    	navigation: {
+    		nextEl: '.swiper-button-next',
+    		prevEl: '.swiper-button-prev',
+    	},
+    	pagination: {
+    		el: '.swiper-pagination',
+    	},
     });
+
+
+$( ".hamburger--slider" ).click(function() {
+				
+				 
+  $( ".hader-nav__menu" ).slideToggle( "slow", function() {
+  });
+});
+			$( ".mobile-menu" ).click(function() {
+  $('.desktop-menu').removeClass( "is-active" );
+});	
+		$( ".desktop-menu" ).click(function() {
+  $('.mobile-menu').addClass( "is-active" );
+});	
